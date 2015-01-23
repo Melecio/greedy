dfab30a6d327688b91d8b4d5ea5aa97532c89525
Melecio Ponte <pontezambrano@gmail.com>
2015-01-23 11:27:25 -0430

    Boceto del 2

---
+++ p1.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+int main(void) {
+    int casos;
+    scanf("%d", &casos);
+
+    while(t--) {
+        int offs, dist, modems, creg, cesp;
+        scanf("%d %d %d %d %d");
+
+        for (int i = 0; i < offs; ++i) {
+            scanf("%d %d");
+
+        }
+
+    }
+}
